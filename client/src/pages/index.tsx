95450edaa83196fb02d8f93b51c1ae3494e73d94
Neezar <rforlyffe@gmaicl.com>
2021-08-13 23:42:45 +0800

    add typescipt and tailwind

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen  bg-gray-600">
+      WHAT
+      <div className="w-40 h-40 bg-red-300"></div>
+    </div>
+  );
+}
