80ad9a3d8dd17d0e8945c5349ffa9900c99277e3
Neezar <rforlyffe@gmaicl.com>
2021-08-22 22:14:04 +0800

    add basic auth

---
+++ server/src/models/user.models.ts
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+// to create a user
+interface UserAttrs {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  profilePic?: string;
+  coverPhoto?: string;
+}
+
+// properties a single user has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  profilePic?: string;
+  coverPhoto?: string;
+}
+
+// entire collection looks like
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attr: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    profilePic: {
+      type: String,
+    },
+    coverPhoto: {
+      type: String,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+});
+
+userSchema.statics.build = (attr: UserAttrs) => {
+  return new User(attr);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
