cd2450bc4badc417d7a056a19e2cb2fc8032860d
Neezar <rforlyffe@gmaicl.com>
2021-08-27 22:38:44 +0800

    add global error

--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -1,6 +1,7 @@
 import { AnyAction, combineReducers, configureStore } from "@reduxjs/toolkit";
 import { combineEpics, createEpicMiddleware } from "redux-observable";
-import authEpic from "./epics/auth.epic";
+import { catchError } from "rxjs/operators";
+import authEpic, { MyEpic } from "./epics/auth.epic";
 import authSlice from "./slices/authSlice";
 
 const reducer = combineReducers({
@@ -24,9 +25,17 @@ export const store = configureStore({
     }).concat(epicMiddleware),
 });
 
-const epics = combineEpics(...authEpic);
+const epics = combineEpics(authEpic);
 
-epicMiddleware.run(epics);
+const rootEpic: MyEpic = (action$, store$, dependencies) =>
+  combineEpics(epics)(action$, store$, dependencies).pipe(
+    catchError((error, source) => {
+      console.error("rootEpic-", error);
+      return source;
+    })
+  );
+
+epicMiddleware.run(rootEpic);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
