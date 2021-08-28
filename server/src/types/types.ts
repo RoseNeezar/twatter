b6a5aa4c3c291398a3f4fd0b2597222214a69644
Neezar <rforlyffe@gmaicl.com>
2021-08-28 22:19:23 +0800

    add custom reqest typing

---
+++ server/src/types/types.ts
@@ -0,0 +1,12 @@
+import * as core from "express-serve-static-core";
+import express from "express";
+
+export interface Query extends core.Query {}
+
+export interface Params extends core.ParamsDictionary {}
+
+export interface RequestTyped<
+  ReqBody = any,
+  ReqQuery = Query,
+  URLParams extends Params = core.ParamsDictionary
+> extends express.Request<URLParams, any, ReqBody, ReqQuery> {}
