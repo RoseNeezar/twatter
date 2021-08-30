4e57b0e4f15b6268631e7b52d465d5e10df4d8fb
Neezar <rforlyffe@gmaicl.com>
2021-08-30 23:07:56 +0800

    refactor tweet page

--- client/src/app/PageComponent/Tweet/components/PostContentContainer.tsx
+++ client/src/app/PageComponent/Tweet/components/PostContentContainer.tsx
@@ -1,32 +1,56 @@
-import React, { FC } from "react";
-import { IPost } from "../../../store/module/post/types/post.types";
+import React, { FC, MouseEvent } from "react";
+import {
+  IPost,
+  RetweetData,
+} from "../../../store/module/post/types/post.types";
 import dayjs from "dayjs";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks/hooks";
 import { likePost, retweetPost } from "../../../store/module/post/post.slice";
 import { selectCurrentUser } from "../../../store/module/auth/auth.slice";
+import Navigate from "../../../utils/Navigate";
+import { useRouteMatch } from "react-router-dom";
+import PostContent from "./PostContent";
 interface IPostContentContainer {
   post: IPost;
+  backUrl: string;
 }
 
 const PostContentContainer: FC<IPostContentContainer> = ({
-  post: { content, createdAt, postedBy, likes, retweetUsers, id, retweetData },
+  post,
+  post: {
+    content,
+    createdAt,
+    postedBy,
+    likes,
+    retweetUsers,
+    id,
+    retweetData,
+    replyTo,
+  },
+  backUrl,
 }) => {
+  const { url } = useRouteMatch();
+
   const dispatch = useAppDispatch();
   const currentUser = useAppSelector(selectCurrentUser);
 
-  const handleLikedPost = () => {
+  const handleLikedPost = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
     dispatch(likePost(id));
   };
 
-  const handleRetweetedPost = () => {
+  const handleRetweetedPost = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
     dispatch(retweetPost(id));
   };
 
-  const handleRetweetLikedPost = () => {
+  const handleRetweetLikedPost = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
     dispatch(likePost(retweetData.id));
   };
 
-  const handleRetweetRetweetedPost = () => {
+  const handleRetweetRetweetedPost = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
     dispatch(retweetPost(retweetData.id));
   };
 
@@ -54,8 +78,22 @@ const PostContentContainer: FC<IPostContentContainer> = ({
     return retweetIndex !== -1 ? true : false;
   };
 
+  const HandleReplyPost = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+    Navigate?.push(`${url}/tweet/${id}`);
+  };
+
+  const HandleToTweet = (event: MouseEvent<HTMLDivElement>) => {
+    console.log("to ?-");
+
+    Navigate?.push(`${backUrl}${postedBy.username}/status/${id}`);
+  };
+
   return (
-    <div className="flex flex-col border-b border-dark-third">
+    <div
+      className="flex flex-col border-b cursor-pointer border-dark-third hover:bg-dark-second"
+      onClick={(event) => HandleToTweet(event)}
+    >
       {retweetData && (
         <div className="flex flex-row items-center text-sm text-gray-500">
           <div className="flex items-center justify-center w-8 h-8 mr-1 rounded-full ">
@@ -65,133 +103,25 @@ const PostContentContainer: FC<IPostContentContainer> = ({
         </div>
       )}
       {retweetData ? (
-        <div className="flex flex-row">
-          <div className="flex justify-center w-20 pt-2 ">
-            <img
-              className="w-10 h-10 rounded-full"
-              src={retweetData.postedBy?.profilePic}
-            />
-          </div>
-          <div className="flex flex-col flex-1 text-dark-txt ">
-            <div className="flex flex-row mt-2">
-              <div className="mr-3 font-bold">
-                {retweetData.postedBy.username}
-              </div>
-              <div className="mr-3 text-gray-500">@{postedBy.email}</div>
-              <div className="text-gray-500">
-                {dayjs(retweetData.createdAt).format("h:mm A	")}
-              </div>
-            </div>
-            <div className=""></div>
-            <div className="py-2 mr-3 break-all">{retweetData.content}</div>
-            <div className="flex flex-row mt-3 text-xl justify-evenly">
-              <div className="flex-1 mt-auto mb-2 -ml-3">
-                <div className="w-12 cursor-pointer hover:text-blue-500">
-                  <div className="flex items-center justify-center w-8 h-8 mr-1 rounded-full hover:bg-opacity-20 hover:bg-blue-300 hover:text-blue-600">
-                    <i className="bx bx-message-rounded"></i>
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex-1 mt-auto mb-2">
-                <div
-                  className={`flex flex-row items-center w-12 cursor-pointer  ${
-                    checkRetweetRetweetedPost() ? "text-green-600" : ""
-                  }`}
-                  onClick={() => handleRetweetRetweetedPost()}
-                >
-                  <div className="flex items-center justify-center w-8 h-8 mr-1 rounded-full hover:bg-opacity-20 hover:bg-green-300 hover:text-green-600">
-                    <i className="bx bx-rotate-right"></i>
-                  </div>
-                  <div className="text-sm">
-                    {retweetData.retweetUsers.length > 0 && (
-                      <p>{retweetData.retweetUsers.length}</p>
-                    )}
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex-1 mt-auto mb-2">
-                <div
-                  className={`flex flex-row items-center w-12 cursor-pointer  ${
-                    checkRetweetLikedPost() ? "text-red-600" : ""
-                  }`}
-                  onClick={() => handleRetweetLikedPost()}
-                >
-                  <div className="flex items-center justify-center w-8 h-8 mr-1 rounded-full hover:bg-opacity-20 hover:bg-red-300 hover:text-red-600">
-                    <i className="bx bx-heart"></i>
-                  </div>
-                  <div className="text-sm ">
-                    {retweetData.likes.length > 0 && (
-                      <p>{retweetData.likes.length}</p>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PostContent
+          post={retweetData}
+          HandleReplyPost={HandleReplyPost}
+          checkLikedPost={checkRetweetLikedPost}
+          checkRetweetedPost={checkRetweetRetweetedPost}
+          handleLikedPost={handleRetweetLikedPost}
+          handleRetweetedPost={handleRetweetRetweetedPost}
+          isRetweet={true}
+        />
       ) : (
-        <div className="flex flex-row">
-          <div className="flex justify-center w-20 pt-2 ">
-            <img
-              className="w-10 h-10 rounded-full"
-              src={postedBy?.profilePic}
-            />
-          </div>
-          <div className="flex flex-col flex-1 text-dark-txt ">
-            <div className="flex flex-row mt-2">
-              <div className="mr-3 font-bold">{postedBy.username}</div>
-              <div className="mr-3 text-gray-500">@{postedBy.email}</div>
-              <div className="text-gray-500">
-                {dayjs(createdAt).format("h:mm A	")}
-              </div>
-            </div>
-            <div className=""></div>
-            <div className="py-2 mr-3 break-all">{content}</div>
-            <div className="flex flex-row mt-3 text-xl justify-evenly">
-              <div className="flex-1 mt-auto mb-2 -ml-3">
-                <div className="w-12 cursor-pointer hover:text-blue-500">
-                  <div className="flex items-center justify-center w-8 h-8 mr-1 rounded-full hover:bg-opacity-20 hover:bg-blue-300 hover:text-blue-600">
-                    <i className="bx bx-message-rounded"></i>
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex-1 mt-auto mb-2">
-                <div
-                  className={`flex flex-row items-center w-12 cursor-pointer  ${
-                    checkRetweetedPost() ? "text-green-600" : ""
-                  }`}
-                  onClick={() => handleRetweetedPost()}
-                >
-                  <div className="flex items-center justify-center w-8 h-8 mr-1 rounded-full hover:bg-opacity-20 hover:bg-green-300 hover:text-green-600">
-                    <i className="bx bx-rotate-right"></i>
-                  </div>
-                  <div className="text-sm">
-                    {retweetUsers.length > 0 && <p>{retweetUsers.length}</p>}
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex-1 mt-auto mb-2">
-                <div
-                  className={`flex flex-row items-center w-12 cursor-pointer  ${
-                    checkLikedPost() ? "text-red-600" : ""
-                  }`}
-                  onClick={() => handleLikedPost()}
-                >
-                  <div className="flex items-center justify-center w-8 h-8 mr-1 rounded-full hover:bg-opacity-20 hover:bg-red-300 hover:text-red-600">
-                    <i className="bx bx-heart"></i>
-                  </div>
-                  <div className="text-sm ">
-                    {likes.length > 0 && <p>{likes.length}</p>}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PostContent
+          post={post}
+          HandleReplyPost={HandleReplyPost}
+          checkLikedPost={checkLikedPost}
+          checkRetweetedPost={checkRetweetedPost}
+          handleLikedPost={handleLikedPost}
+          handleRetweetedPost={handleRetweetedPost}
+          isRetweet={false}
+        />
       )}
     </div>
   );
