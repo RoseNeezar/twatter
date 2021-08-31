f6fd0987527bf81a1401d383c4aa3b3d5139db34
Neezar <rforlyffe@gmaicl.com>
2021-08-31 11:48:58 +0800

    add reply epic

--- client/src/app/store/module/post/post.slice.ts
+++ client/src/app/store/module/post/post.slice.ts
@@ -35,17 +35,63 @@ export const postSlice = createSlice({
         if (post.id === action.payload.id) {
           post.likes = action.payload.likes;
         }
+        if (post.retweetData && post.retweetData.id === action.payload.id) {
+          post.retweetData.likes = action.payload.likes;
+        }
         return post;
       });
+      if (state.replyPost) {
+        if (state.replyPost.postData.id === action.payload.id) {
+          state.replyPost.postData.likes = action.payload.likes;
+        }
+        if (
+          state.replyPost.postData.retweetData &&
+          state.replyPost.postData.retweetData.id === action.payload.id
+        ) {
+          state.replyPost.postData.retweetData.likes = action.payload.likes;
+        }
+      }
+      if (state.replyPost?.replies) {
+        const checker = state.replyPost.replies.findIndex(
+          (re) => re.id === action.payload.id
+        );
+        if (checker !== -1) {
+          state.replyPost.replies.splice(checker, 1, action.payload);
+        }
+      }
     },
     retweetPost: (state, action: PayloadAction<string>) => state,
-    retweetPostFulfilled: (state, action: PayloadAction<IPost>) => state,
+    retweetPostFulfilled: (state, action: PayloadAction<IPost>) => {
+      if (state.replyPost) {
+        if (state.replyPost.postData.id === action.payload.id) {
+          state.replyPost.postData.retweetUsers = action.payload.retweetUsers;
+        }
+        if (
+          state.replyPost.postData.retweetData &&
+          state.replyPost.postData.retweetData.id === action.payload.id
+        ) {
+          state.replyPost.postData.retweetData.retweetUsers =
+            action.payload.retweetUsers;
+        }
+      }
+      if (state.replyPost?.replies) {
+        const checker = state.replyPost.replies.findIndex(
+          (re) => re.id === action.payload.id
+        );
+        if (checker !== -1) {
+          state.replyPost.replies.splice(checker, 1, action.payload);
+        }
+      }
+    },
     getReplyPost: (state, action: PayloadAction<string>) => state,
     getReplyPostFulfilled: (state, action: PayloadAction<IGetReplyPost>) => {
       state.replyPost = action.payload;
     },
     replyToPost: (state, action: PayloadAction<ICreatePost>) => state,
     replyToPostFullfilled: (state, action: PayloadAction<IPost>) => state,
+    replyToSinglePost: (state, action: PayloadAction<ICreatePost>) => state,
+    replyToSinglePostFullfilled: (state, action: PayloadAction<IPost>) => state,
+    updateSinglePost: (state, action: PayloadAction<string>) => state,
   },
 });
 
@@ -62,6 +108,9 @@ export const {
   getReplyPostFulfilled,
   replyToPost,
   replyToPostFullfilled,
+  replyToSinglePost,
+  replyToSinglePostFullfilled,
+  updateSinglePost,
 } = postSlice.actions;
 
 export default postSlice.reducer;
