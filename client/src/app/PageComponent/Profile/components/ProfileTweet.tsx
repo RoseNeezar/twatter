492e54cfdd38bb01e971b158ce2c8f6a0de41594
Neezar <rforlyffe@gmaicl.com>
2021-09-03 23:00:56 +0800

    add profile tab and replies tab, fix ternary logic

--- client/src/app/PageComponent/Profile/components/ProfileTweet.tsx
+++ client/src/app/PageComponent/Profile/components/ProfileTweet.tsx
@@ -1,11 +1,43 @@
-import React, { FC } from "react";
+import React, { FC, MouseEvent, useEffect, useRef, useState } from "react";
+import { useRouteMatch } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks/hooks";
+import { fetchPost } from "../../../store/module/post/post.slice";
+import { fetchProfilePost } from "../../../store/module/user/user.slice";
+import { RootState } from "../../../store/store";
+import PostContentContainer from "../../Tweet/components/PostContentContainer";
 
 interface IProfileTweet {
   backUrl: string;
 }
 
 const ProfileTweet: FC<IProfileTweet> = () => {
-  return <div>ProfileTweet</div>;
+  const { url } = useRouteMatch();
+  const currentUserProfile = useAppSelector(
+    (state: RootState) => state.user.currentUserProfile
+  );
+  const getPost = useAppSelector((state: RootState) => state.user.profilePost);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(
+      fetchProfilePost({ postedBy: currentUserProfile?.id, isReply: false })
+    );
+  }, [dispatch, fetchPost]);
+  return (
+    <div className="flex flex-col w-full">
+      {getPost?.map((res) => {
+        return (
+          <PostContentContainer
+            key={res.id}
+            post={res}
+            backUrl={url}
+            isSinglePost={true}
+            isProfilePost={true}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProfileTweet;
