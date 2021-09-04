bceaa989025edafa75bc8bacac5275b0c03913c3
Neezar <rforlyffe@gmaicl.com>
2021-09-04 23:56:14 +0800

    add search page

---
+++ client/src/app/PageComponent/Search/component/component/SearchAction.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchAction = () => {
+  return (
+    <>
+      <div className="flex items-center justify-between px-4 pt-4">
+        <span className="text-lg font-semibold text-gray-500 dark:text-dark-txt">
+          TBD-SEARCH
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default SearchAction;
