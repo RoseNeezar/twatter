9e7275d70fcf30ab0d80c2fc433f92036d16a376
Neezar <rforlyffe@gmaicl.com>
2021-09-16 19:17:05 +0800

    add create/get channel and scaffold chat routes

---
+++ server/src/controller/chats.controller.ts
@@ -0,0 +1,67 @@
+import { Response } from "express";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Chat, ChatAttrs } from "../models/chat.models";
+import { MessageDoc } from "../models/message.models";
+import { User, UserDoc } from "../models/user.models";
+import { RequestTyped } from "../types/types";
+
+export const createChat = async (
+  req: RequestTyped<{ users: UserDoc[] }, {}, { id: string }>,
+  res: Response
+) => {
+  if (!req.body.users) {
+    console.log("Users param not sent with request");
+    throw new BadRequestError("No user");
+  }
+  const sanitize = req.body.users.map((re) => {
+    return {
+      ...re,
+      _id: re.id,
+    };
+  });
+  const users = sanitize;
+
+  if (users.length === 0) {
+    console.log("Users array is empty");
+    throw new BadRequestError("No user");
+  }
+
+  users.push(req.currentUser as any);
+
+  const chatData = {
+    users: users,
+    isGroupChat: true,
+  };
+
+  Chat.create(chatData)
+    .then((results) => res.status(200).send(results))
+    .catch((error) => {
+      console.log(error);
+      throw new BadRequestError("No user");
+    });
+};
+
+export const getUsersChat = async (
+  req: RequestTyped<{}, { unreadOnly: string }, {}>,
+  res: Response
+) => {
+  Chat.find({ users: { $elemMatch: { $eq: req.currentUser?.id } } })
+    .populate("users")
+    .populate("latestMessage")
+    .sort({ updatedAt: -1 })
+    .then(async (results) => {
+      if (!!req.query.unreadOnly) {
+        results = results.filter((r) => {
+          const newType = r.latestMessage as MessageDoc;
+          return newType && !newType.readBy?.includes(req.currentUser?.id);
+        });
+      }
+
+      results = await User.populate(results, { path: "latestMessage.sender" });
+      res.status(200).send(results);
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new BadRequestError("No user");
+    });
+};
