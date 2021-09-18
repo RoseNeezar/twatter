b31aba4e4d46bde12f0fa763c84ab804ca276504
Neezar <rforlyffe@gmaicl.com>
2021-09-18 16:12:56 +0800

    add bubble style message

--- client/src/app/PageComponent/Messages/components/MessageContent.tsx
+++ client/src/app/PageComponent/Messages/components/MessageContent.tsx
@@ -12,16 +12,32 @@ const MessageContent = React.forwardRef<HTMLDivElement>((prop, ref) => {
   return (
     <div className="relative flex flex-col w-full h-full justify-items-start">
       {chatMessages &&
-        chatMessages?.map((re) => {
+        chatMessages?.map((re, index) => {
           return re.sender.id === currentUser?.id ? (
             <div key={re.id} className="mt-2 ">
-              <div className="p-2 ml-auto mr-3 bg-blue-500 rounded-2xl w-max text-dark-txt">
+              <div
+                className={`max-w-xs p-2 ml-auto mr-3 break-all bg-blue-500  w-max text-dark-txt ${
+                  index === 0
+                    ? "rounded-t-2xl rounded-bl-2xl"
+                    : chatMessages.length - 1 !== index
+                    ? "rounded-l-2xl"
+                    : "rounded-l-2xl rounded-br-2xl"
+                }`}
+              >
                 {re.content}
               </div>
             </div>
           ) : (
             <div key={re.id} className="mt-2 ">
-              <div className="p-2 mr-3 bg-dark-third rounded-2xl w-max text-dark-txt">
+              <div
+                className={`max-w-xs p-2  mr-3 break-all bg-dark-third  w-max text-dark-txt ${
+                  index === 0
+                    ? "rounded-t-2xl rounded-tr-2xl"
+                    : chatMessages.length - 1 !== index
+                    ? "rounded-r-2xl"
+                    : "rounded-r-2xl rounded-bl-2xl"
+                }`}
+              >
                 {re.content}
               </div>
             </div>
